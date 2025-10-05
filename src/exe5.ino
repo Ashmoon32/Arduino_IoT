fb32c176da2277c656c8440c8cab4e20e8321c8d
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-10-05 15:40:23 +0630

    Add Exercise 5

---
+++ src/exe5.ino
@@ -0,0 +1,26 @@
+int buz = 13;
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    int melody[] = {264, 297, 330, 352, 396, 440, 495, 528};
+    tone(13, melody[0]);
+    delay(400);
+    tone(13, melody[1]);
+    delay(400);
+    tone(13, melody[2]);
+    delay(400);
+    tone(13, melody[3]);
+    delay(400);
+    tone(13, melody[4]);
+    delay(400);
+    tone(13, melody[5]);
+    delay(400);
+    tone(13, melody[6]);
+    delay(400);
+    tone(13, melody[7]);
+    delay(400);
+}
